1ea8917175f5ed7b168d2bc9df81dacee16382ac
nvn1176211 <nvnit1176211@gmail.com>
2023-11-24 16:43:28 +0700

    done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let dePopup = document.querySelectorAll('.delightful-experience-img');
 addEventListenerList(dePopup, 'mouseover', showPopup); 
 addEventListenerList(dePopup, 'mouseout', hidePopup); 
+document.getElementById('xs-nav-bar-menu-btn').onclick = function(){
+    let display = document.getElementById('xs-nav-bar-bottom').style.display;
+    document.getElementById('xs-nav-bar-bottom').style.display = display == 'flex'? 'none' : 'flex';
+}
 
 function showPopup(){
     this.querySelectorAll('.delightful-experience-purchase-popup')[0].style.display = 'flex';
